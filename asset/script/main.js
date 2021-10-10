7e203c2b4e81f38dba799a3074d8ab4d1af3dd39
Jiraties <jiratiesstudios@gmail.com>
2021-10-10 16:20:34 +0700

    Developer 1.1 - 10th Oct 2021
    Made appear and gone animations

--- asset/script/main.js
+++ asset/script/main.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector('.hero');
 const slider = document.querySelector('.slider');
-const logo = document.querySelector('#logo');
+const logo = document.querySelector('.logo');
 const hamburger = document.querySelector('.hamburger');
 const headline = document.querySelector('.headline');
 
@@ -24,12 +24,72 @@ tl.fromTo(hero,1,{height: "0%"},{height:"80%", ease: Power2.easeInOut})
 .fromTo(hamburger, 0.5, {opacity: 0, x: 30}, {opacity: 1, x:0}, "-=0.5")
 .fromTo(headline, 0.5, {opacity: 0, x: 30}, {opacity: 1, x:0}, "-=0.5")
 
-window.onscroll = function() {scrollDisapear()};
+const header = document.querySelector("header");
+let moreStatus = true;
+let yesStatus = false;
 
-function scrollDisapear() {
-    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-      tl.fromTo(hero, 0.5, {opacity: 1, height: '100%'}, {opacity: 0, height:'0%'})
-    } else {
-      tl.fromTo(hero, 0.5, {opacity: 0, height:'0%'}, {opacity: 1, height: '100%'})
-    }
-  }
+window.addEventListener('scroll', function () {
+const scrollTop = document.scrollingElement.scrollTop;
+console.log(scrollTop);
+// scrollTop > 300 ? console.log("morethan 300") : console.log(`less than 300`)
+if (scrollTop > 300 && moreStatus === true) {
+  console.log("morethan 300");
+  gone()
+  yesStatus = true;
+  moreStatus = false;
+} else if (scrollTop < 300 && yesStatus === true) {
+  console.log("yes");
+  appear()
+  moreStatus = true;
+  yesStatus = false;
+}
+return
+});
+
+const gone = function () {
+  tl.fromTo(
+    hero,
+    1.2,
+    {width: '80%'},
+    {width: '0%', ease: Power2.easeInOut}
+)
+  .fromTo(
+      headline,
+    0.5,
+    {opacity: 1,},
+    {opacity: 0},
+    "-=1.2"
+)
+  .fromTo(
+    slider,
+    1.2,
+    {x: "0%"},
+    {x:'-100%' , ease: Power2.easeInOut},
+    "-=.9"
+)
+return;
+};
+
+const appear = function () { 
+  tl.fromTo(
+    hero,
+    1.2,
+    {width: '0%'},
+    {width: '80%', ease: Power2.easeInOut}
+)
+  .fromTo(
+      headline,
+    0.5,
+    {opacity: 0,},
+    {opacity: 1},
+    "-=1.2"
+)
+  .fromTo(
+    slider,
+    1.2,
+    {x: "-100%"},
+    {x:'0%' , ease: Power2.easeInOut},
+    "-=.9"
+)
+return;
+};
