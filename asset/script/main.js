f1d5211c921c6d28049379fee64c5999a320c156
Jiraties <jiratiesstudios@gmail.com>
2021-10-11 14:30:01 +0700

    11th Oct 2021 - 1
    Added logo

--- asset/script/main.js
+++ asset/script/main.js
@@ -3,48 +3,14 @@ const slider = document.querySelector('.slider');
 const logo = document.querySelector('.logo');
 const hamburger = document.querySelector('.hamburger');
 const headline = document.querySelector('.headline');
-
-const tl = new TimelineMax();
-
-tl.fromTo(hero,1,{height: "0%"},{height:"80%", ease: Power2.easeInOut}) 
-   .fromTo(
-    hero,
-    1.2,
-    {width: '100%'},
-    {width: '80%', ease: Power2.easeInOut}
-)
-    .fromTo(
-    slider,
-    1.2,
-    {x: "-100%"},
-    {x:'0%' , ease: Power2.easeInOut},
-    "-=1.2"
-)
-.fromTo(logo, 0.5, {opacity: 0, x: 30}, {opacity: 1, x:0}, "-=0.5")
-.fromTo(hamburger, 0.5, {opacity: 0, x: 30}, {opacity: 1, x:0}, "-=0.5")
-.fromTo(headline, 0.5, {opacity: 0, x: 30}, {opacity: 1, x:0}, "-=0.5")
-
+const body = document.querySelector('body');
 const header = document.querySelector("header");
 let moreStatus = true;
 let yesStatus = false;
+const tl = new TimelineMax();
 
-window.addEventListener('scroll', function () {
-const scrollTop = document.scrollingElement.scrollTop;
-console.log(scrollTop);
-// scrollTop > 300 ? console.log("morethan 300") : console.log(`less than 300`)
-if (scrollTop > 300 && moreStatus === true) {
-  console.log("morethan 300");
-  gone()
-  yesStatus = true;
-  moreStatus = false;
-} else if (scrollTop < 300 && yesStatus === true) {
-  console.log("yes");
-  appear()
-  moreStatus = true;
-  yesStatus = false;
-}
-return
-});
+////////////////////////////////////////////////////
+// Functions
 
 const gone = function () {
   tl.fromTo(
@@ -93,3 +59,53 @@ const appear = function () {
 )
 return;
 };
+const wait = function(t) {
+  setTimeout(t);
+  console.log("waited")
+}
+
+
+////////////////////////////////////////////////////
+// Animations
+tl.fromTo(hero,1,{height: "0%"},{height:"80%", ease: Power2.easeInOut}) 
+   .fromTo(
+    hero,
+    1.2,
+    {width: '100%'},
+    {width: '80%', ease: Power2.easeInOut}
+)
+    .fromTo(
+    slider,
+    1.2,
+    {x: "-100%"},
+    {x:'0%' , ease: Power2.easeInOut},
+    "-=1.2"
+)
+.fromTo(logo, 0.5, {opacity: 0, x: 30}, {opacity: 1, x:0}, "-=0.5")
+.fromTo(hamburger, 0.5, {opacity: 0, x: 30}, {opacity: 1, x:0}, "-=0.5")
+.fromTo(headline, 0.5, {opacity: 0, x: 30}, {opacity: 1, x:0}, "-=0.5")
+
+////////////////////////////////////////////////////
+// Main
+wait(1100)
+
+window.addEventListener('scroll', function () {
+const scrollTop = document.scrollingElement.scrollTop;
+console.log(scrollTop);
+// scrollTop > 300 ? console.log("morethan 300") : console.log(`less than 300`)
+if (scrollTop > 200 && moreStatus === true) {
+  body.classList.add("stop-scrolling");
+  console.log("morethan 300");
+  gone()
+  yesStatus = true;
+  moreStatus = false;
+} else if (scrollTop < 200 && yesStatus === true) {
+  console.log("yes");
+  appear()
+  moreStatus = true;
+  yesStatus = false;
+}
+return
+});
+
+
